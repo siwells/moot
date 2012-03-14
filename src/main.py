e71131c8f17ce0b1541ba9fd1ea4abbbd48de157
Simon Wells <siwells@gmail.com>
2012-03-14 09:25:01 +0000

    updated main driver testing out the developing codebase

--- src/main.py
+++ src/main.py
@@ -10,11 +10,11 @@ if __name__ == '__main__':
     white_cstore = Store("cstore", "set", "public", white)
     black = Player("black")
     black_cstore = Store("cstore", "set", "public", black)
-    rules = [ Rule(str(count)) for count in xrange(1,6) ]
+    rules = [ Rule( str(count) ) for count in xrange(1,6) ]
     comp = Composition(turns, part, [black, white], [white_cstore, black_cstore], roles)
     game = Game("dgdl_simple", comp, rules)
     print game.fragment()
     
     
-    mv1 = Interaction("mv1")
+    mv1 = Interaction("mv1", content = ["p","p","p","p"], opener="is it the case that")
     print mv1.fragment()
